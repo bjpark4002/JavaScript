96ba026a7dc3f76c8aa8e868ad329c8297f06f91
Bumjin Park <bjpark4002@gmail.com>
2020-02-06 15:28:08 -0500

    practice update

--- Practice.js
+++ Practice.js
@@ -1,5 +1,5 @@
 
-//#1  output time info
+//Q#1  output time info
 var dayList = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
 var today = new Date();
 
@@ -10,7 +10,7 @@ if(hour >12){
 console.log('\n\n#Q1 . Display time info\n'+'Today is : '+dayList[today.getDay()]+'\nCurrent Time is :' +hour+'PM:'+today.getMinutes()+':'+today.getSeconds()+'\n')
 
 
-//#2 Curretn time.
+//Q#2 Curretn time.
 var month = ''
 
 if(today.getMonth() < 10){
@@ -27,14 +27,17 @@ if(today.getDate()< 10){
 }
 console.log('#Q2 Display Current Date\n'+month+'-'+date+'-'+today.getFullYear()+'\n')
 
-//#Q3 
-function multi(a,b){
-    return a*b
+//Q#10 
+function typeChecker(a ,b){
+ 
+    var result=""
+    if(typeof(a)==typeof(b)){
+        result = "same type"
+    }else{
+        result = "not same"
+    }
+    console.log(result)
 }
-console.log('#Q3 Multiplication \n')
-var var1 = window.prompt("var1")
-var var2 = window.prompt('variable 2')
-
-
+typeChecker(1,'2')
 
 
--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // Code goes here
 
+
+//Q#3
 function mul(){
   
     var field1= document.getElementById("var1").value
@@ -21,9 +23,14 @@ function mul(){
     document.getElementById("answer").innerHTML = ":"+result
   }
   
+
+  //Q#4
   //get current url. window onload is needed to use function after dom is loaded.
   window.onload = function() {document.getElementById("URL").innerHTML=window.location.href}
   
+
+
+  //Q#5
   function letsRev(){
     var tem1 = document.getElementById("var3").value
   
@@ -40,6 +47,8 @@ function compareNumbers(a, b) {
     return a - b;
   }
 
+
+  //Q#6
 function secondSmallBig(){
 
     '1,6,2,3,5'
@@ -62,7 +71,7 @@ function secondSmallBig(){
 }
   
     
-
+//Q#7
 function outputRange(){
     var range1 = parseInt(document.getElementById("range1").value,10)
     var range2 = parseInt(document.getElementById("range2").value,10)
@@ -75,3 +84,96 @@ function outputRange(){
     document.getElementById('rangeAns').innerHTML ="[" +result+"]"
     
   }
+
+  function mergeSortFunction(){
+
+
+  }
+
+
+  //Q#7-a
+
+function merge (left, right) {
+  let result = []
+  let indexLeft = 0
+  let indexRight = 0
+
+  while (indexLeft < left.length && indexRight < right.length) {
+    if (left[indexLeft] < right[indexRight]) {
+      result.push(left[indexLeft])
+      indexLeft++
+    } else {
+      result.push(right[indexRight])
+      indexRight++
+    }
+  }
+
+  return result.concat(left.slice(indexLeft)).concat(right.slice(indexRight))
+}
+
+function mergeSort (arr) {
+  if (arr.length === 1) {
+    return arr
+  }
+
+  const middle = Math.floor(arr.length / 2)  
+  const left = arr.slice(0, middle)  
+  const right = arr.slice(middle)  
+
+  return merge(
+    mergeSort(left),
+    mergeSort(right)
+  )
+}
+  function mergeSortFunction(){
+    var arrayInput = document.getElementById("mergeInput").value
+    arrayInput=arrayInput.substr(1)
+    arrayInput=arrayInput.slice(0,-1)
+    arrayInput=arrayInput.split(",")
+    var i;          //i<5
+    for(i =0 ; i< arrayInput.length; i++){
+    arrayInput[i]=parseInt(arrayInput[i],10)  
+    }
+
+    arrayInput = mergeSort(arrayInput)
+    document.getElementById("mergeAnswer").innerHTML =" ::::: " + arrayInput
+  }
+  //   [1,2,44,3,66,34,21,11]
+  
+  
+  
+  //Q#8
+
+  function findLargest(){
+  
+    var fiveNum = document.getElementById("fiveNumberInput").value
+    
+    var arrayForm = fiveNum.split(',')
+    var i = 0 
+    var result = arrayForm[0];
+    for(i = 1  ; i < 5; i++){
+      if( parseInt(arrayForm[i]) > result){
+        result = parseInt(arrayForm[i]);
+      }
+    }
+    document.getElementById("largestNumber").innerHTML = result
+    
+  }
+
+
+
+  //Q#9
+  function NullChecker(){
+  
+    var nullcheck = document.getElementById("nullchecker").value
+    var result =""
+    if(nullcheck=="")
+      result = "It is Null"
+    else
+      result = "It is not Null"
+    document.getElementById("nullanswer").innerHTML = result
+  }
+
+
+  
+
